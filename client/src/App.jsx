2fce696e50d280711514c255f8ebfe2962bb33e4
Zenitsu-god <pranavlande1@gmail.com>
2024-10-15 14:51:17 +0530

    updated baseUrl

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,8 @@ import UserContextProvider from "./components/userContext";
 import Routes from "./components/routes";
 
 // axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL ;
-axios.defaults.baseURL = "https://dev.eikaze.online/api"
+// axios.defaults.baseURL = "https://dev.eikaze.online/api"
+axios.defaults.baseURL = "https://chatapp-aws-render-final.onrender.com/api"
 axios.defaults.withCredentials = true ;
 
 export default function App() {
