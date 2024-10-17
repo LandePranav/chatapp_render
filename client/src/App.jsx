e6151e7c28417dc96baa27f6e8e0236fec3bc2f4
Zenitsu-god <pranavlande1@gmail.com>
2024-10-17 14:22:42 +0530

    up for murge

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,8 @@ import axios from 'axios' ;
 import UserContextProvider from "./components/userContext";
 import Routes from "./components/routes";
 
-// axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL ;
-// axios.defaults.baseURL = "https://dev.eikaze.online/api"
-// axios.defaults.baseURL = "https://chatapp-aws-render-final.onrender.com/api"
-// axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL ;
-axios.defaults.baseURL = "http://localhost:4040/api";
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL ;
+// axios.defaults.baseURL = 'http://localhost:4040/api'
 axios.defaults.withCredentials = true ;
 
 export default function App() {
